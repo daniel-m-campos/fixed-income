d47ceaefc48eda924fe1c776b583ea63d2fc4e17
Daniel M Campos <daniel.m.campos@icloud.com>
2017-09-17 10:15:09 -0500

    Refactor
    - Add yield to maturity
    - Extract static functions
    - Add equality
    - Add cash flow iterator
    - Add from_price class constructor

--- src/fixed_income/bonds.py
+++ src/fixed_income/bonds.py
@@ -1,5 +1,26 @@
 import math
 
+import scipy.optimize as optimize
+
+
+def future_value_factor(ytm, periods):
+    return math.pow(1 + ytm, periods)
+
+
+def present_value_factor(ytm, periods):
+    return future_value_factor(ytm, -periods)
+
+
+def price(face_value, coupon, periods, ytm):
+    assert isinstance(periods, int)
+    pv_factor = present_value_factor(ytm, periods)
+    annuity_factor = 1 / ytm * (1 - pv_factor) if ytm != 0.0 else periods
+    return coupon * annuity_factor + face_value * pv_factor
+
+
+def yield_to_maturity(bond_price, face_value, periods, coupon, guess=0.05):
+    return optimize.newton(lambda ytm: price(face_value, coupon, periods, ytm) - bond_price, guess)
+
 
 class Bond:
     def __init__(self, face_value, coupon, periods, ytm):
@@ -27,13 +48,12 @@ class Bond:
 
     @property
     def price(self):
-        zero_to_maturity = math.pow(1 + self.ytm, -self.periods)
-        return self._coupon / self.ytm * (1 - zero_to_maturity) + self.face_value * zero_to_maturity
+        return price(self.face_value, self.coupon, self.periods, self.ytm)
 
     @property
     def duration(self):
-        weighted_cf = sum(self.coupon / math.pow(1 + self.ytm, n) for n in range(1, self.periods + 1))
-        weighted_cf += self.periods * self.face_value / math.pow(1 + self.ytm, self.periods)
+        weighted_cf = sum(self.coupon * present_value_factor(self.ytm, n) for n in range(1, self.periods + 1))
+        weighted_cf += self.periods * self.face_value * present_value_factor(self.ytm, self.periods)
         return weighted_cf / self.price
 
     @property
@@ -44,6 +64,21 @@ class Bond:
     def convexity(self):
         # TODO: complete
         return 0.0
+
+    def __eq__(self, other):
+        if isinstance(other, Bond):
+            return all(math.isclose(self.__dict__[key], other.__dict__[key]) for key in self.__dict__)
+        return NotImplemented
+
+    def __iter__(self):
+        for t in range(1, self.periods):
+            yield t, self.coupon
+        yield self.periods, self.coupon + self.face_value
+
+    @classmethod
+    def from_price(cls, bond_price, face_value, coupon, periods):
+        ytm = yield_to_maturity(bond_price=bond_price, face_value=face_value, coupon=coupon, periods=periods)
+        return cls(face_value=face_value, coupon=coupon, periods=periods, ytm=ytm)
 
 
 class ZeroCoupon(Bond):
--- test/test_bonds.py
+++ test/test_bonds.py
@@ -3,7 +3,24 @@ import unittest
 from fixed_income import bonds
 
 
-class TestBonds(unittest.TestCase):
+class TestFunctions(unittest.TestCase):
+    def test_ytm_equals_coupon_rate(self):
+        face_value = 100
+        coupon = 6.5
+        expected = coupon / face_value
+        actual = bonds.yield_to_maturity(bond_price=face_value, face_value=face_value, periods=8, coupon=coupon)
+        self.assertAlmostEqual(actual, expected)
+
+    def test_price_equals_cash_flows_when_ytm_is_zero(self):
+        face_value = 100
+        coupon = 6.5
+        periods = 8
+        expected = coupon * periods + face_value
+        actual = bonds.price(ytm=0, face_value=face_value, periods=periods, coupon=coupon)
+        self.assertAlmostEqual(actual, expected)
+
+
+class TestClasses(unittest.TestCase):
     def test_non_int_periods_causes_assertion_error(self):
         with self.assertRaises(AssertionError):
             bonds.Bond(face_value=100, coupon=5, periods=2.5, ytm=0.03)
@@ -19,3 +36,21 @@ class TestBonds(unittest.TestCase):
         expected = 7
         zcb = bonds.ZeroCoupon(face_value=100, periods=expected, ytm=0.05)
         self.assertAlmostEqual(zcb.duration, expected)
+
+    def test_from_ytm(self):
+        face_value = 100
+        coupon = 6.5
+        periods = 8
+        ytm = coupon / face_value
+        actual = bonds.Bond.from_price(bond_price=face_value, face_value=face_value, periods=periods, coupon=coupon)
+        expected = bonds.Bond(ytm=ytm, face_value=face_value, periods=periods, coupon=coupon)
+        self.assertEqual(actual, expected)
+
+    def test_cashflow_iteration(self):
+        face_value = 100
+        coupon = 7
+        periods = 2
+        bond = bonds.Bond(ytm=0.01, face_value=face_value, periods=periods, coupon=coupon)
+        actual = list(bond)
+        expected = [(1, coupon), (2, coupon + face_value)]
+        self.assertEqual(actual, expected)
