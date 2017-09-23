18b27627897ff1f28900fe111cf012d10146fd19
Daniel M Campos <daniel.m.campos@icloud.com>
2017-09-23 09:27:38 -0500

    Update docstring

--- src/fixed_income/rates.py
+++ src/fixed_income/rates.py
@@ -8,8 +8,10 @@ def compute_days_between(start_date, end_date):
 
 
 def treasury_bill_price(discount_yield, days_to_maturity):
+    """Computes price ot treasury bill"""
     return 100 * (1 - days_to_maturity / 360 * discount_yield)
 
 
 def bond_equivalent_yield(discount_yield, days_to_maturity):
+    """Computes bond equivalent yield from treasury bill discount yield"""
     return 365 * discount_yield / (360 - discount_yield * days_to_maturity)
