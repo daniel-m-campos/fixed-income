9258eb6b87a6163991322daf2c218183225239cf
Daniel M Campos <daniel.m.campos@icloud.com>
2017-11-13 20:19:52 -0600

    Add mortgage module

---
+++ src/fixed_income/mortgages.py
@@ -0,0 +1,30 @@
+import numpy as np
+import pandas as pd
+
+__all__ = ['payments']
+
+
+def payments(loan, maturity, mortgage_rate, freq=1):
+    dt = 1 / freq
+    periods = np.arange(0, int(maturity / dt) + 1)
+    payment_times = periods * dt
+
+    discounts = (1 + mortgage_rate * dt) ** -periods
+    discounts[0] = 0
+
+    payment = loan / np.sum(discounts[1:])  # optimize this with series formula
+
+    balance = 0 * payment_times
+    balance[0] = loan
+    interest = 0 * payment_times
+    amounts = 0 * payment_times
+    for i in periods[1:]:
+        amounts[i] = payment
+        interest[i] = balance[i - 1] * mortgage_rate * dt
+        balance[i] = balance[i - 1] - payment + interest[i]
+    return (pd.DataFrame()
+            .assign(time=payment_times)
+            .assign(value=balance)
+            .assign(payment=amounts)
+            .assign(interest=interest)
+            )
