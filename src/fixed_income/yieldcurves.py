dc6bb51f7d07c4fbe0fca077629051124c85164b
Daniel M Campos <daniel.m.campos@icloud.com>
2017-11-23 18:22:15 -0600

    Add Vasicek to yieldcurves

--- src/fixed_income/yieldcurves.py
+++ src/fixed_income/yieldcurves.py
@@ -2,7 +2,7 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import minimize
 
-__all__ = ['NelsonSiegel']
+__all__ = ['NelsonSiegel', 'Vasicek']
 
 
 def nelson_siegel(theta0, theta1, theta2, kappa, maturities):
@@ -22,16 +22,16 @@ def price_error(real_prices, fitted_prices):
     return ((real_prices - fitted_prices) ** 2).sum()
 
 
-def fit_error(x, real_prices, cashflows, maturities):
+def ns_error(x, real_prices, cashflows, maturities):
     yields = nelson_siegel(*x, maturities=maturities)
     zeros = np.exp(-maturities * yields)
     fitted_prices = price(cashflows, zeros)
     return price_error(real_prices, fitted_prices)
 
 
-def fit(real_prices, cashflows, maturities, x0=None):
+def ns_fit(real_prices, cashflows, maturities, x0=None):
     x0 = x0 if x0 is not None else [0.0, 0.0, 0.0, 1.0]
-    return minimize(fit_error, x0, args=(real_prices, cashflows, maturities), method='powell')
+    return minimize(ns_error, x0, args=(real_prices, cashflows, maturities), method='powell')
 
 
 def forwards(df):
@@ -68,5 +68,57 @@ class NelsonSiegel:
 
     @classmethod
     def from_fit(cls, real_prices, cashflows, cashflow_maturities, x0=None):
-        result = fit(real_prices, cashflows, cashflow_maturities, x0)
+        result = ns_fit(real_prices, cashflows, cashflow_maturities, x0)
         return cls(*result.x)
+
+
+def vasicek(eta, gamma, r0, sigma, maturities):
+    inverse_maturities = (1.0 / maturities).replace(np.inf, 0)
+    sigma2 = pow(sigma, 2)
+    b = 1 / gamma * (1 - np.exp(-gamma * maturities))
+    a = 1 / pow(gamma, 2) * (b - maturities) * (eta * gamma - 0.5 * sigma2) - sigma2 * pow(b, 2) / (4 * gamma)
+    return (-a + b * r0) * inverse_maturities
+
+
+def vasicek_error(x, r0, sigma, real_prices, cashflows, maturities):
+    yields = vasicek(*x, r0=r0, sigma=sigma, maturities=maturities)
+    zeros = np.exp(-maturities * yields)
+    fitted_prices = price(cashflows, zeros).replace(np.inf, 0)
+    return price_error(real_prices, fitted_prices)
+
+
+def vasicek_fit(r0, sigma, real_prices, cashflows, maturities, x0=None):
+    x0 = x0 if x0 is not None else [0.1, 0.1]
+    return minimize(vasicek_error, x0, args=(r0, sigma, real_prices, cashflows, maturities), method='powell')
+
+
+class Vasicek:
+    def __init__(self, eta, gamma, r0, sigma):
+        self.eta = eta
+        self.gamma = gamma
+        self.r0 = r0
+        self.sigma = sigma
+
+    def __repr__(self):
+        params = ",".join(f"{k}={v:.4f}" for k, v in vars(self).items())
+        return f"{self.__class__.__name__}({params})"
+
+    def dataframe(self, maturities):
+        yields = vasicek(self.eta, self.gamma, self.r0, self.sigma, maturities)
+        return (pd.DataFrame()
+                .assign(Maturity=maturities)
+                .assign(Yield=yields)
+                .assign(Zero=np.exp(-maturities * yields))
+                .pipe(forwards)
+                .set_index('Maturity')
+                )
+
+    def price(self, cashflows, cashflow_maturities):
+        yields = vasicek(self.eta, self.gamma, self.r0, self.sigma, cashflow_maturities)
+        zeros = np.exp(-cashflow_maturities * yields)
+        return price(cashflows, zeros)
+
+    @classmethod
+    def from_fit(cls, r0, sigma, real_prices, cashflows, cashflow_maturities, x0=None):
+        result = vasicek_fit(r0, sigma, real_prices, cashflows, cashflow_maturities, x0)
+        return cls(*result.x, r0, sigma)
--- tests/unit/test_yieldcurves.py
+++ tests/unit/test_yieldcurves.py
@@ -6,21 +6,41 @@ import pandas as pd
 
 from fixed_income import yieldcurves
 
-DATA_FILE = f'{os.path.dirname(os.path.abspath(__file__))}/resources/DataTIPS.xlsx'
+DIR = os.path.dirname(os.path.abspath(__file__))
+TIPS_FILE = f'{DIR}/resources/DataTIPS.xlsx'
+VASICEK_FILE = f'{DIR}/resources/VasicekData.xlsx'
 
 
-class TestFit(TestCase):
+class TestNelsonSiegelFit(TestCase):
     def setUp(self):
         super().setUp()
         os.path.dirname(os.path.abspath(__file__))
-        self.quotes = pd.read_excel(DATA_FILE, sheetname='Treasury_Quotes')
+        self.quotes = pd.read_excel(TIPS_FILE, sheetname='Treasury_Quotes')
         self.quotes.index = self.quotes['Time To Maturity'].values
-        self.cashflows = pd.read_excel(DATA_FILE, sheetname='Treasury_Cashflows')
-        self.cf_maturities = pd.read_excel(DATA_FILE, sheetname='Treasury_Cashflows_Maturity')
+        self.cashflows = pd.read_excel(TIPS_FILE, sheetname='Treasury_Cashflows')
+        self.cf_maturities = pd.read_excel(TIPS_FILE, sheetname='Treasury_Cashflows_Maturity')
 
     def test_fit(self):
         prices = (self.quotes['Bid Price'] + self.quotes['Ask Price']) / 2
-        result = yieldcurves.fit(prices, self.cashflows, self.cf_maturities)
+        result = yieldcurves.ns_fit(prices, self.cashflows, self.cf_maturities)
         actual = result.x
         expected = np.array([0.03935294, -0.02175923, -0.07813487, 1.91292469])
+        self.assertTrue(all(np.isclose(actual, expected)))
+
+
+class TestVasicekFit(TestCase):
+    def setUp(self):
+        super().setUp()
+        self.quotes = pd.read_excel(VASICEK_FILE, sheetname='Quotes')
+        self.quotes.index = self.quotes['Maturity'].values
+        self.cashflows = pd.read_excel(VASICEK_FILE, sheetname='CashFlows')
+        self.cf_maturities = pd.read_excel(VASICEK_FILE, sheetname='Maturities')
+
+    def test_fit(self):
+        r0 = 0.011499737607216544
+        sigma = 0.0323
+        prices = (self.quotes['Bid'] + self.quotes['Ask']) / 2 + self.quotes['AccruedInterest']
+        result = yieldcurves.vasicek_fit(r0, sigma, prices, self.cashflows, self.cf_maturities)
+        actual = result.x
+        expected = np.array([0.01342675, 0.03853852])
         self.assertTrue(all(np.isclose(actual, expected)))
