dfc31229acda865b96b12df2b1a763696c62acf2
Daniel M Campos <daniel.m.campos@icloud.com>
2017-12-16 14:01:17 -0600

    Make month constants for clarity

--- src/fixed_income/futures.py
+++ src/fixed_income/futures.py
@@ -1,5 +1,7 @@
 import numpy as np
 
+MONTHS_IN_QUARTER = 3
+MONTHS_IN_YEAR = 12
 CONVERSION_YIELD = 0.06
 CONVERSION_FV = 1.03
 
@@ -8,10 +10,10 @@ def conversion_factor(security_type, coupon, time_to_maturity):
     years = np.floor(time_to_maturity)
     year_fraction = time_to_maturity - years
     if security_type in ('TY', 'US', 'UB'):
-        months = np.floor(year_fraction / 3 * 12) * 3
-        v = months if months < 7 else 3
+        months = np.floor(year_fraction / MONTHS_IN_QUARTER * MONTHS_IN_YEAR) * MONTHS_IN_QUARTER
+        v = months if months < 7 else MONTHS_IN_QUARTER
     else:
-        months = np.floor(year_fraction * 12)
+        months = np.floor(year_fraction * MONTHS_IN_YEAR)
         v = months if months < 7 else months - 6
     a = 1 / pow(CONVERSION_FV, v / 6)
     b = (coupon / 2) * (6 - v) / 6
